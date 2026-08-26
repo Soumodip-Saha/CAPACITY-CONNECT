80987a0509cb9dbdc69429feb85b0650d277d3ec
Soumodip-Saha <123857386+Soumodip-Saha@users.noreply.github.com>
2026-08-26 13:21:19 +0530

    Fix error

--- app/main.py
+++ app/main.py
@@ -46,6 +46,9 @@ app.include_router(admin_router)
 app.include_router(api_router)
 
 templates = Jinja2Templates(directory="app/templates")
+@app.get("/login", response_class=HTMLResponse)
+def redirect_to_auth_login():
+    return RedirectResponse(url="/auth/login", status_code=303)
 
 @app.get("/", response_class=HTMLResponse)
 def index_homepage(request: Request):
